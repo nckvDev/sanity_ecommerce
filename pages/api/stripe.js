34f139e0dbfe79d20e33502711e65c5c26bea68c
nckvDev <jirapong.ku@rmuti.ac.th>
2023-04-03 15:20:43 +0700

    fix: connect sanity

--- pages/api/stripe.js
+++ pages/api/stripe.js
@@ -11,23 +11,23 @@ export default async function handler(req, res) {
         payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-          { shipping_rate: 'shr_1Kn3IaEnylLNWUqj5rqhg9oV' },
+          { shipping_rate: 'shr_1MshxbGYi3limTUy2rY2kIWh' },
         ],
         line_items: req.body.map((item) => {
           const img = item.image[0].asset._ref;
-          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/6wwtksp1/production/').replace('-webp', '.webp');
 
           return {
-            price_data: { 
-              currency: 'usd',
-              product_data: { 
+            price_data: {
+              currency: 'thb',
+              product_data: {
                 name: item.name,
                 images: [newImage],
               },
               unit_amount: item.price * 100,
             },
             adjustable_quantity: {
-              enabled:true,
+              enabled: true,
               minimum: 1,
             },
             quantity: item.quantity
